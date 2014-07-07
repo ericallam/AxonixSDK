3b6271dabda3d0d5bba7c63071362ceb8aaddbb8
Eric Allam <eallam@icloud.com>
2014-07-07 14:19:51 +0100

    Added a header and lib

---
+++ AxonixSDK.h
@@ -0,0 +1,13 @@
+//
+//  AxonixSDK.h
+//  AxonixSDK
+//
+//  Created by Eric Allam on 07/07/2014.
+//  Copyright (c) 2014 Axonix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AxonixSDK : NSObject
+
+@end
